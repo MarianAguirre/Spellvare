e7669bad8212f1490c75950d89df4e08642afb67
Marian <marian.aguirre.carrill@gmail.com>
2025-10-06 12:53:44 -0600

    Se genera la traduccion de texto a braille

--- src/app/pages/traductor-page/traductor-page.component.ts
+++ src/app/pages/traductor-page/traductor-page.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslationService } from './service/translation.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-traductor-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './traductor-page.component.html',
   styleUrl: './traductor-page.component.css'
 })
 export class TraductorPageComponent {
+    private translationService = inject(TranslationService)
+
+    text: string = '';
+    translation: string = '';
+
+    brailleMap: { [key: string]: string } = {
+      'a': '⠁', 'b': '⠃', 'c': '⠉', 'd': '⠙', 'e': '⠑',
+      'f': '⠋', 'g': '⠛', 'h': '⠓', 'i': '⠊', 'j': '⠚',
+      'k': '⠅', 'l': '⠇', 'm': '⠍', 'n': '⠝', 'o': '⠕',
+      'p': '⠏', 'q': '⠟', 'r': '⠗', 's': '⠎', 't': '⠞',
+      'u': '⠥', 'v': '⠧', 'w': '⠺', 'x': '⠭', 'y': '⠽',
+      'z': '⠵', "":"   ", " ":"   "
+    };
+    convertirText() {
+      this.translation = '';
+      const textLower = this.text.toLowerCase();
+      for (let caracter of textLower) {
+        this.translation += this.brailleMap[caracter] || caracter;
+      }
+      this.translationService.setTranslation(this.text, this.translation);
+    console.log(this.text, this.translation)
+    this.text = '';
+    }
+
 
 }
