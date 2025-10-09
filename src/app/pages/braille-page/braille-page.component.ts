7c9f136ec9375bfc4f7f235b6dea51e1698fc2d3
Marian <marian.aguirre.carrill@gmail.com>
2025-10-09 13:31:38 -0600

    Traduccion de braille a español

--- src/app/pages/braille-page/braille-page.component.ts
+++ src/app/pages/braille-page/braille-page.component.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ButtonHomeComponent } from "../../components/button-home/button-home.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-braille-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, ButtonHomeComponent],
+  imports: [RouterLink, RouterLinkActive, ButtonHomeComponent, CommonModule],
   templateUrl: './braille-page.component.html',
   styleUrl: './braille-page.component.css'
 })
 export class BraillePageComponent {
 
+  brailleMap: Record<string, string> = {
+    '⠁': 'a', '⠃': 'b', '⠉': 'c', '⠙': 'd', '⠑': 'e',
+    '⠋': 'f', '⠛': 'g', '⠓': 'h', '⠊': 'i', '⠚': 'j',
+    '⠅': 'k', '⠇': 'l', '⠍': 'm', '⠝': 'n', '⠕': 'o',
+    '⠏': 'p', '⠟': 'q', '⠗': 'r', '⠎': 's', '⠞': 't',
+    '⠥': 'u', '⠧': 'v', '⠺': 'w', '⠭': 'x', '⠽': 'y',
+    '⠵': 'z', ' ': ' '
+  };
+
+  // brailleChars = [
+  //   ['⠁','⠃','⠉','⠙'],
+  //   ['⠑','⠋','⠛','⠓'],
+  //   ['⠊','⠚','⠅','⠇'],
+  //   ['⠍','⠝','⠕','⠏'],
+  //   ['⠟','⠗','⠎','⠞'],
+  //   ['⠥','⠧','⠺','⠭'],
+  //   ['⠽','⠵',' ']
+  // ];
+
+  brailleChars = [
+    ['⠁','⠃','⠉','⠙', '⠑','⠋','⠛','⠓', '⠊','⠚','⠅','⠇', '⠍','⠝','⠕','⠏', '⠟','⠗','⠎','⠞', '⠥','⠧','⠺','⠭', '⠽','⠵',' ']
+  ];
+
+  text = '';
+  translation = '';
+
+  addBrailleChar(brailleChar: string): void {
+    const translated = this.brailleMap[brailleChar] ?? '';
+    this.text += translated;
+    this.translation = this.text;
+  }
+
+  clear(): void {
+    this.text = '';
+    this.translation = '';
+  }
 }
